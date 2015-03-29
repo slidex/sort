c2028c1308e4dfb012aa6d9d785eebff6cdfdaab
Nick K <nickk@slidex.com>
2015-03-30 00:31:20 +0200

    added pop()

--- heapsort.js
+++ heapsort.js
@@ -12,14 +12,14 @@ function toString(array, index, depth) {
     depth = depth || 0;
 
     if (index < array.length) {
-    	for (i = 0; i < depth; i++) {
-    		str += '| ';
-    	} 
+        for (i = 0; i < depth; i++) {
+            str += '| ';
+        } 
         str += array[index] + '\n';
     }
 
-	depth++;
-	
+    depth++;
+    
     leftIndex = index * 2 + 1;
     if (leftIndex < array.length) {
         str += toString(array, leftIndex, depth);
@@ -34,31 +34,69 @@ function toString(array, index, depth) {
 }
 
 function add(array, value) {
-	var index,
-	    parentIndex,
-	    tmp;
-	
-	if (!array) {
-		return;
-	}
-	
-	array.push(value);
+    var index,
+        parentIndex,
+        tmp;
+    
+    if (!array) {
+        return;
+    }
+    
+    array.push(value);
 
-	for (index = array.length - 1; ; ) {
-		if (index === 0) {
-			break;
-		}
-		parentIndex = (index - (index % 2 == 1 ? 1 : 2)) / 2;
-		if (array[index] > array[parentIndex]) {
-			tmp = array[index];
-			array[index] = array[parentIndex];
-			array[parentIndex] = tmp;
-		}
-		index = parentIndex;
-	}
+    for (index = array.length - 1; ;) {
+        if (index === 0) {
+            break;
+        }
+        parentIndex = (index - (index % 2 == 1 ? 1 : 2)) / 2;
+        if (array[index] > array[parentIndex]) {
+            tmp = array[index];
+            array[index] = array[parentIndex];
+            array[parentIndex] = tmp;
+        }
+        index = parentIndex;
+    }
 }
 
-var a = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+function pop(array, length) {
+    var value = array[0],
+        index,
+        leftIndex,
+        rightIndex,
+        biggerChildIndex,
+        tmp;
+    
+    maxIndex = (length || array.length) - 1;
+    array[0] = array[maxIndex];
+    array[maxIndex] = 'x';
+    
+    for (index = 0; ;) {
+        leftIndex = index * 2 + 1;
+        rightIndex = index * 2 + 2;
+        
+        if (leftIndex >= maxIndex && rightIndex >= maxIndex) {
+            break;
+        } else if (rightIndex >= maxIndex) {
+            biggerChildIndex = array[leftIndex];
+        } else {
+            biggerChildIndex = array[leftIndex] > array[rightIndex] ? leftIndex : rightIndex;
+        }
+        
+        if (array[biggerChildIndex] < array[index]) {
+            break;
+        }
+        
+        tmp = array[index];
+        array[index] = array[biggerChildIndex];
+        array[biggerChildIndex] = tmp;
+        
+        index = biggerChildIndex;
+    }
+    
+    return value;
+}
+
+var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 //console.log(toString(a));
 
 var heap = [];
@@ -68,4 +106,6 @@ add(heap, 2);
 add(heap, 3);
 add(heap, 4);
 add(heap, 5);
+pop(heap);
+pop(heap);
 console.log(toString(heap));
